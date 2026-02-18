f0148f3950c0076822674fff07a1d4b1a2d25bfe
unknown <m.lebon@rubika-edu.re>
2026-02-18 16:01:52 +0400

    Correction du reflect

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -3,26 +3,39 @@ using UnityEngine.UIElements;
 
 public class BallBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        transform.Translate(new Vector2(0, 10));
-    }
+    public int bounces = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
+    private void OnDrawGizmos()
+    {
+        Vector2 origin = transform.position;
+        Vector2 dir = transform.right;
+        Ray ray = new Ray(origin, dir);
+
+        Gizmos.DrawLine(origin, origin + dir);
+
+        for (int i = 0; i < bounces; i++)
+        {
+
+
+            if ((Physics.Raycast(ray, out RaycastHit hit)))
+            {
+                Gizmos.DrawSphere(hit.point, 0.1f);
+                Vector2 reflected = Reflect(ray.direction, hit.normal);
+                Gizmos.color = Color.Lerp(Color.white, Color.red, i / bounces);
+                Gizmos.DrawLine(hit.point, (Vector2)hit.point + reflected);
+                ray.direction = reflected;
+                ray.origin = hit.point;
+
+
+            }
+            else break;
+        }
+
+    }
 
     private Vector2 Reflect(Vector2 d, Vector2 n)
     {
         return d - 2 * Vector2.Dot(n, d) * n;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Reflect(gameObject.transform.position, collision.gameObject.transform.position);
     }
 }
